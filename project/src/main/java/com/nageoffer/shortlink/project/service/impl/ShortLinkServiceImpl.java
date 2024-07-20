fdba293f04f6e97d5730729246e01ea22b434a33
Hzwei087 <1074429495@qq.com>
2024-07-20 22:39:48 +0800

    feature: 短链接跳转时如果已过有效期，则将该短链接设置为空，防止缓存穿透

--- project/src/main/java/com/nageoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -224,9 +224,18 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                     .eq(ShortLinkDO::getEnableStatus, 0);
             ShortLinkDO shortLinkDO = baseMapper.selectOne(shortLinkDOqueryWrapper);
             if (shortLinkDO != null) {
+                if (shortLinkDO.getValidDate() != null && shortLinkDO.getValidDate().before(new Date())){
+                    stringRedisTemplate.opsForValue().set(String.format(RedisKeyConstant.GOTO_IS_NULL_SHORT_LINK_KEY, fullShortUrl),"-",30, TimeUnit.SECONDS);
+                    return;
+                }
                 String originUrl = shortLinkDO.getOriginUrl();
-                stringRedisTemplate.opsForValue().set(redisKey, originUrl);
+                stringRedisTemplate.opsForValue().set(
+                        redisKey,
+                        originUrl,
+                        LinkUtil.getLinkCacheValidDate(shortLinkDO.getValidDate()),
+                        TimeUnit.MILLISECONDS);
                 ((HttpServletResponse) response).sendRedirect(originUrl);
+
 
             }
 
